0ecf9a91e97b26197e6bbe4b35c2aa0caa4b382d
robstell <87148681+robstell@users.noreply.github.com>
2021-08-27 18:30:20 -0300

    Add files via upload

---
+++ ListaSelecionadaPersistente.java
@@ -0,0 +1,13 @@
+package pacote_products_find;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
+public class ListaSelecionadaPersistente implements Serializable{
+    
+    
+    
+    public ArrayList<String> listaSelecionadaPersistente;
+    
+    
+ }
